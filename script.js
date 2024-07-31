48b80a3dc204d45d0067e5dbc6f16ab51c08e32d
alterhumanite <emberonpawss@gmail.com>
2024-07-31 15:25:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//let mdp=prompt("mot de passe?");
+//if (mdp=="KITCAT"){
+//	alert("Bienvenue");
+//}
+//else{
+//	alert("mauvais mot de passe...");
+//}
+//let prenom=prompt("Quel est ton prenom ?");
+//let titre=document.getElementById("titre");
+//titre.textContent="Bonjour "+prenom;
+
+//let paragraphe=document.createElement("p");ssssssssaaaaa
+//paragraphe.innerText="Hello world!";
+//document.getElementById("boite").appendChild(paragraphe);
